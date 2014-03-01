974e302072424713e8e3737df771e656b56a21f7
Marko Mijuskovic <marko.mijuskovic@gmail.com>
2014-03-01 10:34:08 -0500

    Added controllers.

---
+++ Source/Twitta.Website/Controllers/ApplicationsController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Mvc;
+using Twitta.Website.Logic;
+using Twitta.Website.Models;
+
+namespace Twitta.Website.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private readonly ITwitterAppLogic _apiApplicationLogic;
+
+        public ApplicationsController(ITwitterAppLogic apiApplicationLogic)
+        {
+            _apiApplicationLogic = apiApplicationLogic;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View(_apiApplicationLogic.GetItems());
+        }
+
+        public ActionResult Create()
+        {
+            return View(new TwitterApp());
+        }
+
+        [HttpPost]
+        public ActionResult Create(TwitterApp model)
+        {
+            if (ModelState.IsValid)
+            {
+                _apiApplicationLogic.SaveOrUpdate(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(_apiApplicationLogic.GetItem(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TwitterApp model)
+        {
+            if (ModelState.IsValid)
+            {
+                _apiApplicationLogic.SaveOrUpdate(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_apiApplicationLogic.GetItem(id));
+        }
+	}
+}
---
+++ Source/Twitta.Website/ViewModels/SearchReportView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Twitta.Website.Models;
+
+namespace Twitta.Website.ViewModels
+{
+    public class SearchReportView
+    {
+        public Search Search { get; set; }
+        public IEnumerable<KeyValuePair<string, int>> Data { get; set; }
+    }
+}
