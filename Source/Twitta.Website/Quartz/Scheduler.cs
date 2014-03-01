bdfb8061f93f7089b18d72aabb648831e16fe11b
psantiago <Peter.J.Santiago@gmail.com>
2014-03-01 09:34:14 -0500

    added quartz service

--- Source/Twitta.Website/Quartz/Scheduler.cs
+++ Source/Twitta.Website/Quartz/Scheduler.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using Quartz.Impl;
+using Quartz.Impl.Triggers;
 
 namespace Twitta.Website.Quartz
 {
-    public class Scheduler
+    public static class Scheduler
     {
+        public static void Init()
+        {
+            var scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            var job = new JobDetailImpl("SearchJob", "jobs", typeof(SearchJob));
+            var trigger = new CronTriggerImpl("SearchTrigger", "jobs", ConfigurationManager.AppSettings["CronTrigger"]);
+            scheduler.ScheduleJob(job, trigger);
+        }
     }
 }
--- Source/Twitta.Website/Quartz/SearchJob.cs
+++ Source/Twitta.Website/Quartz/SearchJob.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
+using Quartz;
 
 namespace Twitta.Website.Quartz
 {
-    public class SearchJob
+    [DisallowConcurrentExecution]
+    public class SearchJob : IJob, IRegisteredObject
     {
+        private readonly object _lock = new object();
+        private bool _shuttingDown = false;
+
+        public void Execute(IJobExecutionContext context)
+        {
+            //TODO - implement calling the actua repo/logic methods
+        }
+
+        public void Stop(bool immediate)
+        {
+            lock (_lock)
+            {
+                _shuttingDown = true;
+            }
+            HostingEnvironment.UnregisterObject(this);
+        }
     }
 }
